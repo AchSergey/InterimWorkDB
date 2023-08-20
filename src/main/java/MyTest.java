332d187c1153c468874478ba935de2dc3018c15e
AchSergey <bubonik_70@mail.ru>
2023-08-20 10:27:36 +0300

    Пробую отправить

--- src/main/java/MyTest.java
+++ src/main/java/MyTest.java
@@ -2,5 +2,6 @@ public class MyTest {
     public static void main(String[] args) {
         System.out.println("Проверка");
         System.out.println("Проверка не удалась!");
+        System.out.println("Не правда! Всё норм.");
     }
 }
